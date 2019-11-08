79c08f435edeb349a237a78acc9bf84395a4099c
li <liyu52577@163.com>
2019-11-08 08:23:34 +0800

    code update

--- src/engine/sqlengine.py
+++ src/engine/sqlengine.py
@@ -15,7 +15,7 @@ from sqlalchemy.orm import sessionmaker
 class SqlEngine(object):
 
     def __init__(self, destination_db):
-        self.con = sa.create_engine(destination_db)
+        self.engine = sa.create_engine(destination_db)
         self.session = self.create_session()
 
     def __del__(self):
@@ -23,12 +23,12 @@ class SqlEngine(object):
             self.session.close()
 
     def create_session(self):
-        dest_session = sessionmaker(bind=self.con, autocommit=False, autoflush=True)
+        dest_session = sessionmaker(bind=self.engine, autocommit=False, autoflush=True)
         return dest_session()
 
     def fetch_data(self, table_name):
         df_list = []
-        for chunk in pd.read_sql(table_name, self.con, chunksize=1000000):
+        for chunk in pd.read_sql(table_name, self.engine, chunksize=1000000):
             df_list.append(chunk)
-        res = pd.concat(df_list, ignore_index=True)
-        return res
+        df_data = pd.concat(df_list, ignore_index=True)
+        return df_data
